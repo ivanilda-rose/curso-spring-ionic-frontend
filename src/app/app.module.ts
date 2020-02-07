410015a4e3843ef87f5cb3a779dad944c3cbc70a
Ivanilda Almeida <1229172+ivanilda-rose@users.noreply.github.com>
2020-02-07 12:58:15 -0100

    Mostrando dados e imagem do cliente para pagina de profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
 
 import { MyApp } from './app.component';
 
@@ -32,7 +33,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     CategoriaService,
     ErrorInterceptorProvider,
     AuthService,
-    StorageService
+    StorageService,
+    ClienteService
   ]
 })
 export class AppModule {}
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the ProfilePage page.
@@ -16,22 +19,37 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 	
-  email: string;
+  cliente : ClienteDTO
 
   constructor(
 	  public navCtrl: NavController, 
 	  public navParams: NavParams,
-	  public storage: StorageService
+	  public storage: StorageService,
+	  public clienteService: ClienteService
   ) {
   }
 
   ionViewDidLoad() {
 	let localUser =  this.storage.getLocalUser();
 	if (localUser && localUser.email) {
-		this.email = localUser.email;
-		
-	} 
-    
+		this.clienteService.findByEmail(localUser.email).subscribe(
+			response => {
+				this.cliente = response;
+				this.getImageIfExists();				
+			},
+			error => {}		
+		);
+	}
+  }
+  
+  getImageIfExists(){	  
+	 return this.clienteService.getImageFromBucket(this.cliente.id).subscribe(
+		response => {
+			this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+			
+		},
+		error =>{}
+	 );	  
   }
 
 }
