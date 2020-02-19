656f857a0ef6439ae5e5b27b4e7a822a50fc2249
Ivanilda Almeida <1229172+ivanilda-rose@users.noreply.github.com>
2020-02-19 12:41:42 -0100

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpRequest, HttpHandler, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx';
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
@@ -32,9 +33,15 @@ export class ErrorInterceptor implements HttpInterceptor{
 				case 401:
 				this.handle401();
 				break;
+				
 				case 403:
 				this.handle403();
-				break;	
+				break;
+				
+				case 422:
+				this.handle422(errorObj);
+				break;
+				
 				default:
 				this.handleDefaultError(errorObj);			
 			}
@@ -62,6 +69,22 @@ export class ErrorInterceptor implements HttpInterceptor{
 		this.storage.setLocalUser(null);		
 	}
 	
+	handle422(errorObj){
+		let alert = this.alertCtrl.create({
+				title: 'Erro 422: Validação',
+				message: this.listErrors(errorObj.errors),
+				enableBackdropDismiss: false,
+				buttons: [
+					{
+						text: 'Ok'
+					}
+				]
+			
+		});
+		alert.present();
+			
+	}
+	
 	handleDefaultError(errorObj){
 		let alert = this.alertCtrl.create({
 			title: 'Erro: ' + errorObj.status + ':' + errorObj.error,
@@ -75,6 +98,15 @@ export class ErrorInterceptor implements HttpInterceptor{
 		})
 		alert.present();
 	}
+	
+	listErrors(messages : FieldMessage[]) : string {
+		let s : string = '';
+		for (var i=0; i < messages.length; i++){
+			s = s +	'<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+		}
+		return s;
+	}
+	
 }
 
 export const ErrorInterceptorProvider = {
