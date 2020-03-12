746b435c55db39cc17c511f283d2d4c37a1437d8
Ivanilda Almeida <1229172+ivanilda-rose@users.noreply.github.com>
2020-03-12 15:24:57 -0100

    Tela de confirmacao de pedido

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -3,13 +3,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PedidoDTO } from '../../models/pedido.dto';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-/**
- * Generated class for the PaymentPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-payment',
@@ -37,9 +30,9 @@ export class PaymentPage {
 	}
 
 	nextPage(){
-		console.log("Type: "+ this.formGroup.value['@type']);
 		this.pedido.pagamento = this.formGroup.value;
-		console.log(this.pedido);		
+		this.navCtrl.setRoot('OrderConfirmationPage', {pedido: this.pedido});
+		
 	}
 
 }
