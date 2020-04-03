83bcdb4f24c20b5e51aeb7205600af9025e11535
Ivanilda Almeida <1229172+ivanilda-rose@users.noreply.github.com>
2020-04-03 15:24:19 -0100

    Fazendo upload da foto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ClienteService } from '../services/domain/cliente.service';
 import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 import { ProdutoService } from '../services/domain/produto.service';
 import { CartService } from '../services/domain/cart.service';
+import { ImageUtilService } from '../services/image-util.service';
 
 import { MyApp } from './app.component';
 
@@ -40,7 +41,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     StorageService,
     ClienteService,
     ProdutoService,
-    CartService
+    CartService,
+    ImageUtilService
   ]
 })
 export class AppModule {}
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -34,6 +34,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+	this.loadData();
+  }
+  
+  loadData(){
 	let localUser =  this.storage.getLocalUser();
 	if (localUser && localUser.email) {
 		this.clienteService.findByEmail(localUser.email).subscribe(
@@ -78,5 +82,19 @@ export class ProfilePage {
 		 this.cameraOn = false;
 		}, (err) => {		 
 	});			  
+  }
+  
+  sendPicture() {
+	this.clienteService.uploadPicture(this.picture).subscribe(
+		response => {
+			this.picture = null;
+			this.loadData();				
+		},
+		error => {
+		});  
+  }
+  
+  cancel() {
+	this.picture = null;
   }
 }
