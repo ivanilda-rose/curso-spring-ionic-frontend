2c56fb837deb1e88afc34111f8ba545d0e16bef0
Ivanilda Almeida <1229172+ivanilda-rose@users.noreply.github.com>
2020-05-12 12:11:56 -0100

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-	baseUrl: "http://192.168.1.85:8080",
+	baseUrl: "http://curso-javaspring-ionic.herokuapp.com",
 	bucketBaseUrl: "https://curso-javaspring-ionic.s3.eu-west-3.amazonaws.com"
 
 }
