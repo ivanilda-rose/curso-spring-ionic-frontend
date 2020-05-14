8f487b9362839bf8a935678b3fd887488f49f8da
Ivanilda Almeida <1229172+ivanilda-rose@users.noreply.github.com>
2020-05-14 14:33:24 -0100

    Bugfix - atualizacao da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { ClienteService } from '../../services/domain/cliente.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { API_CONFIG } from '../../config/api.config';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 /**
  * Generated class for the ProfilePage page.
@@ -22,16 +23,18 @@ export class ProfilePage {
 	
   cliente : ClienteDTO;
   picture : string;
-  cameraOn : boolean = false;
+  profileImage;
+  cameraOn : boolean = false;  
 
   constructor (
 	  public navCtrl: NavController, 
 	  public navParams: NavParams,
 	  public storage: StorageService,
 	  public clienteService: ClienteService,
-	  public camera: Camera
+	  public camera: Camera,
+	  public sanitizer: DomSanitizer
   ) {
-	 
+	  this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -60,13 +63,28 @@ export class ProfilePage {
   getImageIfExists() {	  
 	 return this.clienteService.getImageFromBucket(this.cliente.id).subscribe(
 		response => {
-			this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;			
+			this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+			this.blobToDataURL(response).then(dataUrl => {
+			    let str : string = dataUrl as string;
+				this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+			})
 		},
-		error =>{			
+		error =>{
+			this.profileImage = 'assets/imgs/avatar-blank.png';
 		}
 	 );	  
   }
-    
+  
+  blobToDataURL(blob) {
+	return new Promise((fulfill, reject) => {
+		let reader = new FileReader();
+		reader.onerror = reject;
+		reader.onload = (e) => fulfill(reader.result);
+		reader.readAsDataURL(blob);		  
+	})
+	  
+  }
+  
   getCameraPicture() {	  
     this.cameraOn = true;
 	  
@@ -113,7 +131,7 @@ export class ProfilePage {
 	this.clienteService.uploadPicture(this.picture).subscribe(
 		response => {
 			this.picture = null;
-			this.loadData();				
+			this.getImageIfExists();				
 		},
 		error => {
 		});  
